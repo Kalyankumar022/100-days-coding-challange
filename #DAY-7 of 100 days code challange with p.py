bd59c3807d4af0575721670b113c93ef3c28dd7c
Kalyankumar022 <123871496+Kalyankumar022@users.noreply.github.com>
2023-12-25 22:37:06 +0530

    #Day7

---
+++ #DAY-7 of 100 days code challange with p.py
@@ -0,0 +1,42 @@
+#DAY-7 of 100 days code challange with python
+
+a = int(10)
+b = int(20)
+
+if a<b:
+        print("True")
+else:
+        print("False")
+        
+name = "Chatrapathi Sivaji"
+Count_word = len(name)
+first_letter = (name[0])
+print(first_letter)
+second_letter = name[Count_word-1]
+print(second_letter)
+
+#just i am changing the code
+
+n = "kalyan kumar"
+Count_len = len(n)
+print(Count_len)
+printing_my_num = int(Count_len)
+print(printing_my_num)
+print(type(printing_my_num))
+printing_my_num = str(printing_my_num)
+print(type(printing_my_num))
+
+
+my_first_number = int(printing_my_num[0])
+print(type(my_first_number))
+print(my_first_number)
+my_second_number = int(printing_my_num[1])
+print(type(my_second_number))
+
+as_my_result = my_first_number<my_second_number
+
+if as_my_result:
+        print("True Statement")
+
+else:
+        print("State ment is not correct")
